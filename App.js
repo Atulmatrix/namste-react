d64f9654d3e8af819ddf5e85568cf67a518de8a2
Utkarsh Sharma <atulsharma@accuraticssoftwares.com>
2024-01-16 14:03:34 -0600

    Igniting the app

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading=React.createElement("h1",
 // {id:"heading"},
 // "Hello World from React");
+// console.log(heading); //this will print a react element which is a normal javascript object.
 // const root= ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-
+// ReactElement(JavaScript Object)=>HTML(Browser Understand)
 
 
 {/* <div id="parent">
@@ -27,8 +31,12 @@
 const parent=React.createElement("div",
 {id:"parent"},
 [React.createElement("div",{id:"child1"},
-[React.createElement("h1",{},"I am an h1 tag"),React.createElement("h2",{},"I am an h2 tag"),]),
+[React.createElement("h1",{},"I am an h1 tagggg"),React.createElement("h2",{},"I am an h2 tag"),]),
 React.createElement("div",{id:"child2"},
 [React.createElement("h1",{},"I am an h1 tag"),React.createElement("h2",{},"I am an h2 tag"),])]);
 const root= ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent); //object
 root.render(parent);
+
+// JSX
