9ee87e07f7c815e8a15b5f096024551b32aacc29
Atul Sharrma <atulsharma@accuraticssoftwares.com>
2024-01-17 08:28:57 -0600

    listtle update Laying the foundation

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ console.log(heading); //object yes
 
 // JSX has HTML like syntax but its not HTML.
 // JSX(Babel transpile into react code before it reaches to JS Engine)-Done by Parcel and Babel.
-// JSX=>Babel tranpileit to React.createElement=> React Element -JS object => HTML Element after rendering
+// JSX=>Babel tranpile it to React.createElement=> React Element -JS object => HTML Element after rendering
 
-const jsxheading=(<h1 id="heading" className="head">Namste React using JSX</h1>)
+const jsxheading=(<h1 id="heading" className="head">Namste React using JSX</h1>);
 console.log(jsxheading); //object=>React Object
 const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxheading);
