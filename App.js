aed04448a5ec20d6dfa1087dff6d27d9f0964516
Atul Sharrma <atulsharma@accuraticssoftwares.com>
2024-01-17 18:04:43 -0600

    Laying the foundation Part04

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{id:"heading"}, "namste JS");
+// ReactElement
+const heading = (
+  <h1 id="heading" className="head">
+    Namste React using JSX
+  </h1>
+);
 
-// React.createElement=> React Element -JS object => HTML Element after rendering
+//React Functional Component
 
+// const fn = () => true;
+// const fn2 = () => {
+//   return true;
+// };
+// const ReactComponent = () => {
+//   return <h1>Namste React using React Componenet</h1>;
+// };
+// const ReactComponent1 = () => <h1>Namste React using React Componenet</h1>;
 
-console.log(heading); //object yes
-
-// JSX has HTML like syntax but its not HTML.
-// JSX(Babel transpile into react code before it reaches to JS Engine)-Done by Parcel and Babel.
-// JSX=>Babel tranpile it to React.createElement=> React Element -JS object => HTML Element after rendering
-
-const jsxheading=(<h1 id="heading" className="head">Namste React using JSX</h1>);
-console.log(jsxheading); //object=>React Object
-const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+const Title = () => (
+  <h1 id="heading" className="head">
+    Namste Ttile Component
+  </h1>
+);
+//Component composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="New">Namste React using React Component</h1>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
