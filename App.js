d50913c349f426aa4c88eea122c3dba7d1b94bd8
Atul Sharrma <atulsharma@accuraticssoftwares.com>
2024-01-17 18:54:56 -0600

    Laying the foundation Part5

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
 // ReactElement
+
+const heading0 = (
+  <h1 id="zero" className="head">
+    Namste React using JSXOrigin
+  </h1>
+);
 const heading = (
   <h1 id="heading" className="head">
+    {heading0}
     Namste React using JSX
   </h1>
 );
@@ -21,13 +28,25 @@ const heading = (
 
 const Title = () => (
   <h1 id="heading" className="head">
-    Namste Ttile Component
+    Namste Title Component
   </h1>
 );
+
+// const Title1 = function () {
+//   return (
+//     <h1 id="heading" className="head">
+//       Namste Ttile Component
+//     </h1>
+//   );
+// };
+
 //Component composition
 const HeadingComponent = () => (
   <div id="container">
     <Title />
+    <Title></Title>
+    {Title()}
+    {heading}
     <h1 className="New">Namste React using React Component</h1>
   </div>
 );
