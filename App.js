0ca33acd0da6779856f166d6c416249e94c5ff21
Atul Sharrma <atulsharma@accuraticssoftwares.com>
2024-01-18 05:23:50 -0600

    - | Talk is Cheap, Show Me the Code-Part01

--- App.js
+++ App.js
@@ -1,54 +1,90 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-// ReactElement
+// Appcomponenet
+// header
+// -Logo
+// -Nav
 
-const heading0 = (
-  <h1 id="zero" className="head">
-    Namste React using JSXOrigin
-  </h1>
-);
-const heading = (
-  <h1 id="heading" className="head">
-    {heading0}
-    Namste React using JSX
-  </h1>
-);
+// body
+// -Search
+// -Restaurant container
+// ---RestaurantCard
 
-//React Functional Component
+// footer
+// -CopyRigt
+// -Links
+// -Address
+// -Contact
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          src="https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705449600&semt=ais"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// const fn = () => true;
-// const fn2 = () => {
-//   return true;
-// };
-// const ReactComponent = () => {
-//   return <h1>Namste React using React Componenet</h1>;
-// };
-// const ReactComponent1 = () => <h1>Namste React using React Componenet</h1>;
+const StyleCard = { backgroundColor: "yellow" };
 
-const Title = () => (
-  <h1 id="heading" className="head">
-    Namste Title Component
-  </h1>
-);
+const RestaurantCard = () => {
+  return (
+    // <div className="cover" style={StyleCard}>
+    <div className="cover" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/d3711387c3cb4ff9eaff53e1653cb46d"
+        alt=""
+      />
+      <h3>Paneer Shop</h3>
+      <h4>Biryani, North India,Paneer</h4>
+      <h4>4.8 Star</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
 
-// const Title1 = function () {
-//   return (
-//     <h1 id="heading" className="head">
-//       Namste Ttile Component
-//     </h1>
-//   );
-// };
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="SearchContainer">Search</div>
+      <div className="res-Container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-//Component composition
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <Title></Title>
-    {Title()}
-    {heading}
-    <h1 className="New">Namste React using React Component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
