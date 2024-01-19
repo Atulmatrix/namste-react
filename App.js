122c3ed02a1bb30142d55a8b617ae6b31faf663f
Atul Sharrma <atulsharma@accuraticssoftwares.com>
2024-01-19 09:05:49 -0600

    passing single restaurant object

--- App.js
+++ App.js
@@ -40,20 +40,75 @@ const Header = () => {
 const StyleCard = { backgroundColor: "yellow" };
 
 const RestaurantCard = (props) => {
-  const { resName, cuisine } = props;
+  const { resName } = props;
   return (
     // <div className="cover" style={StyleCard}>
     <div className="cover" style={{ backgroundColor: "#f0f0f0" }}>
       <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/d3711387c3cb4ff9eaff53e1653cb46d"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resName.cloudinaryImageId
+        }
         alt=""
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
+      <h3>{resName.name}</h3>
+      <h4>{resName.cuisines.join(", ")}</h4>
       <h4>4.8 Star</h4>
       <h4>38 minutes</h4>
     </div>
   );
+};
+
+const resObject = {
+  id: "177495",
+  name: "La Pino'z Pizza",
+  cloudinaryImageId: "ugemttaiukyqfhxkfs9i",
+  locality: "Pathways School Road",
+  areaName: "Hajipur",
+  costForTwo: "₹300 for two",
+  cuisines: ["Pizzas", "Pastas", "Italian", "Desserts", "Beverages"],
+  avgRating: 3.9,
+  parentId: "4961",
+  avgRatingString: "3.9",
+  totalRatingsString: "10K+",
+  sla: {
+    deliveryTime: 32,
+    lastMileTravel: 4.3,
+    serviceability: "SERVICEABLE",
+    slaString: "32 mins",
+    lastMileTravelString: "4.3 km",
+    iconType: "ICON_TYPE_EMPTY",
+  },
+  availability: {
+    nextCloseTime: "2024-01-19 06:00:00",
+    opened: true,
+  },
+  badges: {},
+  isOpen: true,
+  type: "F",
+  badgesV2: {
+    entityBadges: {
+      imageBased: {},
+      textBased: {},
+      textExtendedBadges: {},
+    },
+  },
+  aggregatedDiscountInfoV3: {
+    header: "₹120 OFF",
+    subHeader: "ABOVE ₹199",
+    discountTag: "FLAT DEAL",
+  },
+  differentiatedUi: {
+    displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    differentiatedUiMediaDetails: {
+      mediaType: "ADS_MEDIA_ENUM_IMAGE",
+      lottie: {},
+      video: {},
+    },
+  },
+  reviewsSummary: {},
+  displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  restaurantOfferPresentationInfo: {},
 };
 
 const Body = () => {
@@ -61,8 +116,8 @@ const Body = () => {
     <div className="body">
       <div className="SearchContainer">Search</div>
       <div className="res-Container">
-        <RestaurantCard resName="Mera Dhaba" cuisine="Mutton-Biryani" />
-        <RestaurantCard resName="Tera Dhaba" cuisine="Butter paneer masala" />
+        <RestaurantCard resName={resObject} />
+        <RestaurantCard resName={resObject} />
       </div>
     </div>
   );
